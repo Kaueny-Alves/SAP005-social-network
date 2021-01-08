72ba91df709a35ea2ffbe433c0f4921903ce611d
Kaueny Alves <kaueny@MacBook-Pro-de-Kaueny.local>
2021-01-08 10:32:51 -0300

    funcão de criar poste e armazenar no firestore funcionando

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -7,31 +7,31 @@ export const Register = () => {
     <div id="main-container">
      <h1>Efetue seu cadastro</h1>
      <form id="registrer-form">
-      <div clas="full-box">
+      <div class="full-box">
         <label for="email"> E-mail</label>
         <input type="email" name="email" id="email" placeholder="Digite seu email">
       </div>
-      <div clas="half-box spacing">
+      <div class="half-box spacing">
         <label for="name"> Nome</label>
         <input type="name" name="name" id="name" placeholder="Digite seu nome" >
       </div>
-      <div clas="half-box">
+      <div class="half-box">
         <label for="lastname"> Sobrenome</label>
         <input type="lastname" name="lastname" id="lastname" placeholder="Digite seu sobrenome">
       </div>
-      <div clas="half-box spacing">
+      <div class="half-box spacing">
         <label for="password"> Senha</label>
         <input type="password" name="password" id="password" placeholder="Digite sua senha">
       </div>
-      <div clas="half-box">
+      <div class="half-box">
         <label for="passconfirmation"> Confirme sua senha</label>
         <input type="password" name="passconfirmation" id="passconfirmation" placeholder="Confirme sua senha">
       </div>  
-      <div clas="full-box">
+      <div class="full-box">
         <input type="checkbox" name="agreement" id="agreement">
         <label for="agreement" id="agreement-label">Eu li e aceito os <a href="#">termos de uso.</a> </label>
       </div>   
-      <div clas="full-box">
+      <div class="full-box">
         <input type="submit" id="btn-submit" value="Cadastrar">
       </div> 
     </form>
