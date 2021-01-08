9e3a6ab2b8df61cdd48077e53c02af19c2b86de9
Kaueny Alves <kaueny@MacBook-Pro-de-Kaueny.local>
2021-01-08 20:42:11 -0300

    ajustes de css

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -1,5 +1,3 @@
-// import { getPosts } from '../../services/index.js';
-
 export const Feed = () => {
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
@@ -13,13 +11,6 @@ export const Feed = () => {
      </section>
   </div>
       `;
-
-  // const submit = rootElement.querySelector('#btn-post');
-
-  // submit.addEventListener('load', () => {
-  //   const getPost = rootElement.querySelector('#postsAll');
-  //   getPosts(getPost);
-  // });
 
   return rootElement;
 };
