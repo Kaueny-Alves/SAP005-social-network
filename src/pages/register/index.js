1a7fa6365d93eecbdeea2f46e8a0009cf32a736c
Kaueny Alves <kaueny@MacBook-Pro-de-Kaueny.local>
2021-01-15 19:23:38 -0300

    Função de deletar funcionando

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -11,72 +11,39 @@ export const Register = () => {
         <label for="email"> E-mail</label>
         <input type="email" name="email" id="email" placeholder="Digite seu email" data-min-length="3">
       </div>
-      <div class="half-box spacing">
+      <div class="full-box">
         <label for="name"> Nome</label>
         <input type="name" name="name" id="name" placeholder="Digite seu nome" >
       </div>
-      <div class="half-box">
+      <div class="full-box">
         <label for="lastname"> Sobrenome</label>
         <input type="lastname" name="lastname" id="lastname" placeholder="Digite seu sobrenome">
       <div class="half-box spacing">
       <label for="talents"> Qual seu talento? </label>
       <input type="Talents" name="talents" id="talents" placeholder= " Digite seu talento" >
     </div>
-      <div class="half-box spacing">
+      <div class="full-box ">
         <label for="password"> Senha</label>
         <input type="password" name="password" id="password" placeholder="Digite sua senha">
       </div>
-      <div class="half-box">
-        <label for="passconfirmation"> Confirme sua senha</label>
-        <input type="password" name="passconfirmation" id="passconfirmation" placeholder="Confirme sua senha">  
-      </div>    
       <div class="full-box">
         <input type="submit" id="btn-submit" value="Cadastrar">
       </div> 
     </form>
   
-  <p class="error-validation template"></p>
+  
   </div>
     `;
 
-
- /* class Validator{
-
-    constructor(){
-      this.validations = [
-        'data-min-length',
-      ]
-    }
-    validate(form){ //iniciar validação em todos os campos
-
-      let inputs =form.getElementsByTagName('input');
-
-      let inputsArray = [...inputs];
-      
-
-    }
-    
-    
-  }  
-
-  const form = rootElement.querySelector('#register-form');*/
   const submit = rootElement.querySelector('#btn-submit');
 
   submit.addEventListener('click', (e) => {
-   
     e.preventDefault();
 
     const email = rootElement.querySelector('#email').value;
     const password = rootElement.querySelector('#password').value;
     const name = rootElement.querySelector('#name').value;
     const lastName = rootElement.querySelector('#lastname').value;
-
-
-          /* if (errorCode === 'auth/wrong-password' || errorCode === 'auth/invalid-email') {
-        return alert('Email ou Senha incorreta!');
-      }
-    /* se email digitado igual email return cadastrado
-nos imput colocar obrigatório email e senha */
 
     createUser(email, password, name, lastName);
   });
--- src/services/index.js
+++ src/services/index.js
@@ -1,123 +1,127 @@
 export const createUser = (email, password, name, lastName, talents = 'cantar') => {
-    firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then(cred => {
-            cred.user.updateProfile({ displayName: name + lastName })
-            firebase
-                .firestore()
-                .collection('Usuarios')
-                .doc()
-                .set({ name, lastName, talents });
-            console.log('usuário criado com sucesso');
-            alert('Usuário cadastrado com sucesso');
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log('deu ruim', errorMessage, errorCode);
-            if (errorCode === 'auth/email-already-in-use') {
-                return alert('Email e-mail já cadastrado');
-            }
-            if (errorCode === 'auth/weak-password') {
-                return alert('A senha deve ter no minimo 6 caracteres');
-            }
-            return `Codigo de error: ${errorCode}`;
-        });
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((cred) => {
+      const user = firebase.auth.currentUser;
+      const uidUser = user.uid;
+      cred.user.updateProfile({ displayName: `${name} ${lastName}` });
+      firebase
+        .firestore()
+        .collection('Usuarios')
+        .doc(uidUser)
+        .set({ name, lastName, talents });
+      console.log('usuário criado com sucesso');
+      alert('Usuário cadastrado com sucesso');
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log('deu ruim', errorMessage, errorCode);
+      if (errorCode === 'auth/email-already-in-use') {
+        return alert('Email e-mail já cadastrado');
+      }
+      if (errorCode === 'auth/weak-password') {
+        return alert('A senha deve ter no minimo 6 caracteres');
+      }
+      return `Codigo de error: ${errorCode}`;
+    });
 };
 
 export const userLogin = (email, password) => {
-    firebase
-        .auth()
-        .signInWithEmailAndPassword(email, password)
-        .then((result) => {
-            const token = result.user.uid;
-            const user = result.user;
-            console.log(user, token, 'usuário Logado com sucesso');
-            return (token, user);
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log('deu ruim', errorMessage, errorCode);
-            if (errorCode === 'auth/wrong-password' || errorCode === 'auth/invalid-email') {
-                return alert('Email ou Senha incorreta!');
-            }
-            return `Codigo de error: ${errorCode}`;
-        });
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((result) => {
+      const token = result.user.uid;
+      const user = result.user;
+      console.log(user, token, 'usuário Logado com sucesso');
+      return (token, user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log('deu ruim', errorMessage, errorCode);
+      if (errorCode === 'auth/wrong-password' || errorCode === 'auth/invalid-email') {
+        return alert('Email ou Senha incorreta!');
+      }
+      return `Codigo de error: ${errorCode}`;
+    });
 };
 
 export const gmailLogin = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider)
-        .then((result) => {
-            const token = result.credential.accessToken;
-            const user = result.user;
-            console.log(user, 'Usuário Logado com o google com sucesso', token);
-            return (user, token);
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            const email = error.email;
-            const credential = error.credential;
-            console.log('deu ruim,Usuário não foi logado', errorCode, errorMessage, email, credential);
-        });
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+    .then((result) => {
+      const token = result.credential.accessToken;
+      const user = result.user;
+      console.log(user, 'Usuário Logado com o google com sucesso', token);
+      return (user, token);
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = error.credential;
+      console.log('deu ruim,Usuário não foi logado', errorCode, errorMessage, email, credential);
+    });
 };
 
 export const userLogout = () => {
-    firebase
-        .auth()
-        .signOut()
-        .then(() => {
-            console.log('usuário deslogado com sucesso');
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log('deu ruim, usuário continua logado', errorMessage, errorCode);
-        });
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      console.log('usuário deslogado com sucesso');
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log('deu ruim, usuário continua logado', errorMessage, errorCode);
+    });
 };
 
 export const createPost = (post) => {
-    const db = firebase.firestore();
-    db.collection('Posts')
-        .add({
-            user: firebase.auth().currentUser.displayName,
-            post,
-            likes: [],
-            comments: [],
-        }).then(
-            console.log('post criado com sucesso'),
-        )
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log('deu ruim, post não foi criado.', errorMessage, errorCode);
-        });
+  const db = firebase.firestore();
+  db.collection('Posts')
+    .add({
+      uidPost: firebase.auth().currentUser.uid,
+      user: firebase.auth().currentUser.displayName,
+      post,
+      likes: [],
+      comments: [],
+    }).then(
+      console.log('post criado com sucesso'),
+    )
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log('deu ruim, post não foi criado.', errorMessage, errorCode);
+    });
 };
 
-// export const getPosts = () => firebase.firestore()
-//   .collection('Posts')
-//   .get()
-//   .then((snapshot) => snapshot.forEach((doc) => (doc.data())));
-
 export const getPosts = () => (
-    firebase
+  firebase
     .firestore()
     .collection('Posts')
     .get()
     .then((result) => result.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
+      id: doc.id,
+      ...doc.data(),
     })))
 );
 
 export const getUsers = () => {
-    const dbUsers = firebase.firestore().collection('Usuarios').get();
+  const dbUsers = firebase.firestore().collection('Usuarios').get();
 
-    dbUsers.then((snapshot) => {
-        snapshot.forEach((doc) => {
-            console.log(doc.data());
-            return doc.data();
-        });
+  dbUsers.then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.data());
+      return doc.data();
     });
+  });
+};
+
+export const deletePost = () => {
+
+  const postDelete = firebase.firestore().collection('Post').doc(id);
+  return postDelete.delete();
 };
